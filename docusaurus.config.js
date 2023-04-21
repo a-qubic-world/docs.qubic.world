45cefc48f6807e9e80e1a34e8ed2f099d21a641d
frog-rabbit <thefrograbbit@gmail.com>
2023-04-21 21:23:16 +0200

    removed editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,17 +40,12 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/', // change routeBasePath to '/'
-          // Please change this to your repo.
+
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/a-qubic-world/docs.qubic.world',
+          // editUrl: 'https://github.com/a-qubic-world/docs.qubic.world',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
