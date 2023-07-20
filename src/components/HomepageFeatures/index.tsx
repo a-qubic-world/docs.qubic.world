d667328379cfb8e2aa89b5a219cc97b26c8f0134
frog-rabbit <thefrograbbit@gmail.com>
2023-07-20 16:00:49 +0200

    removed demo images

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,38 +4,46 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  //Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Learn about Qubic',
+    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Dive into the world of Qubic, a revolutionary crypto platform that's reshaping the digital landscape. 
+        With its unique quorum-based computation (QBC) system, Useful Proof-of-Work, and feeless transfers, 
+        Qubic sets a new standard for reliability, efficiency, and user-friendliness. 
+        Explore how Qubic combines the best aspects of existing platforms and eradicates their limitations."
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Become a Computor',
+    //Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Take part in the future of blockchain technology. By becoming a Computor in the Qubic network, 
+        you're not just a participant, but a pillar of our innovative consensus protocol. 
+        Maintain high computational standards, execute smart contracts and contribute to the 
+        decentralization and security of the Qubic platform. 
+        Discover how you can leverage your computing prowess for rewards and play a pivotal role in the Qubic ecosystem.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Become an Investor',
+    //Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Seize the opportunity to invest in one of the most promising crypto platforms on the horizon. 
+        Qubic's unique approach to consensus, mining, and smart contract execution not only sets it apart 
+        but also ensures a scalable and robust platform. By investing in Qubic, you're not just backing a 
+        cryptocurrency; you're fuelling an advanced, sustainable, and game-changing technology. 
+        Learn more about the potential of Qubic and embark on an exciting investment journey.
       </>
     ),
   },
@@ -44,9 +52,11 @@ const FeatureList: FeatureItem[] = [
 function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
+      {/*
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
+      */}
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
