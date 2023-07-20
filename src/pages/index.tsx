b2497a3bc380c99dd382f1767f5282095665b905
frog-rabbit <thefrograbbit@gmail.com>
2023-07-20 16:00:15 +0200

    modified first link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/learn/overview">
+            to="/overview/introduction">
             Get started
           </Link>
         </div>
@@ -30,7 +30,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
